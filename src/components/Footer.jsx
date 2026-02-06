9fc2c02e4f70d8e48e21050277c15d644e43aaca
DaikiARAI20030526 <araidaiki2003@gmail.com>
2026-02-06 14:35:13 +0900

    footer調整

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     // - border-top 0.3px #D9D9D9 追加
     // - text-white 削除
     // - text-left (左寄せ)
-    <footer className="border-t-[0.3px] border-[#D9D9D9] py-8 px-4 text-left">
+    <footer className="border-t-[0.3px] border-[#FFD014] py-8 px-4 text-left">
       {/* ■ 修正点: 
           - text-[10px]
           - text-[#D9D9D9]
